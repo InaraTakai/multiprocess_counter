70b844982a2d462009f19b8886725a67660b107f
Inara <inaratakashi@gmail.com>
2020-08-08 05:00:22 -0300

    Lab funcionando

--- src/main.c
+++ src/main.c
@@ -3,19 +3,20 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int num_primo(int x){
-  int i, resultado;
+int num_primo(int x){ /* Função que determina se número é primo */
+  int i, num_dividores;
 
-  resultado = 0;
+  num_dividores = 0;
 
   for (i=1;i<=x;i++){
     if (x % i == 0){
-      resultado++;
+      num_dividores++; /* Caso o numero seja divisivel pelo índice i, incrementa número de divisores */
     }
   }
 
-  if (resultado == 2){
+  if (num_dividores == 2){ /* Número tem 2 divisores (1 e ele mesmo) */
     return 1;
   }else{
     return 0;
@@ -23,35 +24,30 @@ int num_primo(int x){
 }
 
 int main() {
-  pid_t filho[4];
+  pid_t filho;
   int numero;
   char caracter;
   int num_processos = 0;
   int i,aux;
+
+  aux = 0;
 
   /* Definir flags de protecao e visibilidade de memoria */
   int protection = PROT_READ | PROT_WRITE;
   int visibility = MAP_SHARED | MAP_ANON;
 
   /* Criar area de memoria compartilhada */
-  int *b;
-  b = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
-  if ((long int)b==-1) printf("Erro de alocacao!\n");
-  (*b)=0;
+  int *qtd_primos;
+  qtd_primos = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
+  (*qtd_primos)=0;
 
   while (aux == 0){
-    while (num_processos < 4){
+    if (num_processos < 4){ /* Se o numero de processor é menor que 4, realiza a leitura da entrada e cria um processo */
       scanf("%d%c", &numero, &caracter);
-      printf("num=%d caracter=%c\n", numero, caracter);
-
-      filho[num_processos] = fork();
-      printf("eita:%d\n", num_processos);
-      if (filho[num_processos] == 0) {
-        /* Esta parte do codigo executa no processo filho */
-        printf("num=%d num_processo=%d\n", numero, num_processos);
-        (*b) += num_primo(numero);
-        printf("qtd=%d\n", *b);
-        exit(0);
+      filho = fork();
+      if (filho == 0) {
+        (*qtd_primos) += num_primo(numero); /* Na execução do processo filho verifica se número é primo */
+        exit(0); /* Sai do processo após verificação */
       }
       num_processos++;
 
@@ -59,19 +55,17 @@ int main() {
         aux = 1;
         break;
       }
-    }
-    for(i=0;i<4;i++){
-      waitpid(filho[num_processos], NULL, 0);
-      num_processos = 0;
+    }else {
+      waitpid(-1, NULL, 0); /* Caso o número de processor aberto seja 4, aguarda até um dos processos terminar */
+      num_processos--;
     }
   }
 
   for(i=0;i<4;i++){
-    printf("ENCERA\n");
-    waitpid(filho[num_processos], NULL, 0);
+    waitpid(-1, NULL, 0); /* Aguarda todos os processos encerrarem  */
   }
 
-  printf("%d\n", *b);
+  printf("%d\n", *qtd_primos);
 
   return 0;
 }
